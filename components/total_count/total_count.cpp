2a2ccc523d3da18b798a164cf0d537667733f788
Sebastian Muszynski <s.muszynski@constructiva.de>
2022-05-14 20:50:46 +0200

    Clean up

--- components/total_count/total_count.cpp
+++ components/total_count/total_count.cpp
@@ -9,15 +9,11 @@ static const char *const TAG = "total_count";
 void TotalCount::setup() {
   uint32_t initial_value = this->initial_value_;
 
-  if (this->restore_) {
+  if (this->restore_ && this->total_count_sensor_ != nullptr) {
     this->pref_ = global_preferences->make_preference<float>(this->total_count_sensor_->get_object_id_hash());
     this->pref_.load(&initial_value);
   }
   this->publish_state_and_save(initial_value);
-
-  this->total_count_ = initial_value;
-  this->last_update_ = millis();
-  this->last_save_ = this->last_update_;
 
   this->parent_->add_on_state_callback([this](bool state) { this->process_new_state_(state); });
 }
@@ -28,28 +24,37 @@ void TotalCount::dump_config() {
 }
 
 void TotalCount::publish_state_and_save(uint32_t state) {
+  this->last_update_ = millis();
+  this->total_count_ = state;
+
   this->publish_state_(this->total_count_sensor_, state);
   this->publish_state_(this->total_count_number_, state);
-  const uint32_t now = millis();
-  if (now - this->last_save_ < this->min_save_interval_) {
-    return;
+
+  if (this->restore_ && this->total_count_sensor_ != nullptr) {
+    const uint32_t now = millis();
+    if (now - this->last_save_ < this->min_save_interval_) {
+      return;
+    }
+    this->last_save_ = now;
+
+    this->pref_.save(&state);
   }
-  this->last_save_ = now;
-  this->pref_.save(&state);
 }
 
-void TotalCount::reset_counter() { this->publish_state_and_save(this->initial_value_); }
+void TotalCount::reset_counter() {
+  this->publish_state_and_save(this->initial_value_);
+}
 
-void TotalCount::set_value(float value) { this->publish_state_and_save((uint32_t) value); }
+void TotalCount::set_value(float value) {
+  this->publish_state_and_save((uint32_t) value);
+}
 
 void TotalCount::process_new_state_(bool state) {
   if (std::isnan(state))
     return;
 
   if (state) {
-    this->total_count_ = this->total_count_ + this->step_;
-    this->last_update_ = millis();
-    this->publish_state_and_save(this->total_count_);
+    this->publish_state_and_save(this->total_count_ + this->step_);
   }
 }
 
