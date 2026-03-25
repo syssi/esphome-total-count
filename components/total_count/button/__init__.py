17d2b043fd040b1b71154a8a691c1e1b2b2120e1
Sebastian Muszynski <basti@linkt.de>
2026-03-25 22:21:01 +0100

    Fix lint issues

--- components/total_count/button/__init__.py
+++ components/total_count/button/__init__.py
@@ -1,7 +1,6 @@
 import esphome.codegen as cg
 from esphome.components import button
 import esphome.config_validation as cv
-from esphome.const import CONF_ID
 
 from .. import CONF_TOTAL_COUNT_ID, TOTAL_COUNT_COMPONENT_SCHEMA, total_count_ns
 
@@ -29,7 +28,6 @@ async def to_code(config):
     for key in [CONF_RESET_COUNTER]:
         if key in config:
             conf = config[key]
-            var = cg.new_Pvariable(conf[CONF_ID])
+            var = await button.new_button(conf)
             await cg.register_component(var, conf)
-            await button.register_button(var, conf)
             cg.add(var.set_parent(hub))
